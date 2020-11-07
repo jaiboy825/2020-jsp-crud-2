525accab3aee59f7c2838d0ed20ebdcb51ee2317
jaiboy825 <jaiboy@naver.com>
2020-11-07 13:15:44 +0900

    11월 7일 crud 프로젝트 시작

---
+++ Book20121/WebContent/book.sql
@@ -0,0 +1,15 @@
+create table book_tbl (
+	bcode number(5) primary key not null,
+	btitle varchar2(30),
+	bwriter varchar2(30),
+	bpub number(4),
+	bprice number(10) not null,
+	bdate date
+)
+
+insert into book_tbl values(10100, '자바킹', '강길동', 1001, 12000, '20201102');
+insert into book_tbl values(10101, '알고리듬', '남길동', 1002, 18000, '20200505');
+insert into book_tbl values(10102, '스프링두', '서길동', 1003, 23000, '20190803');
+insert into book_tbl values(10103, '파이썬', '홍길동', 1004, 9000, to_date()'20191011');
+
+select * from book_tbl;
---
+++ Book20121/src/book/BookVO.java
@@ -0,0 +1,77 @@
+package book;
+
+import java.sql.Date;
+
+public class BookVO {
+	private int bcode;
+	private String btitle;
+	private String bwriter;
+	private int bpub;
+	private int bprice;
+	private Date bdate;
+
+	public BookVO(int bcode, String btitle, String bwriter, int bpub, int bprice, Date bdate) {
+		super();
+		this.bcode = bcode;
+		this.btitle = btitle;
+		this.bwriter = bwriter;
+		this.bpub = bpub;
+		this.bprice = bprice;
+		this.bdate = bdate;
+	}
+
+	public int getBcode() {
+		return bcode;
+	}
+
+	public void setBcode(int bcode) {
+		this.bcode = bcode;
+	}
+
+	public String getBtitle() {
+		return btitle;
+	}
+
+	public void setBtitle(String btitle) {
+		this.btitle = btitle;
+	}
+
+	public String getBwriter() {
+		return bwriter;
+	}
+
+	public void setBwriter(String bwriter) {
+		this.bwriter = bwriter;
+	}
+
+	public int getBpub() {
+		return bpub;
+	}
+
+	public void setBpub(int bpub) {
+		this.bpub = bpub;
+	}
+
+	public int getBprice() {
+		return bprice;
+	}
+
+	public void setBprice(int bprice) {
+		this.bprice = bprice;
+	}
+
+	public Date getBdate() {
+		return bdate;
+	}
+
+	public void setBdate(Date bdate) {
+		this.bdate = bdate;
+	}
+
+	@Override
+	public String toString() {
+		return "BookVO [bcode=" + bcode + ", btitle=" + btitle + ", bwriter=" + bwriter + ", bpub=" + bpub + ", bprice="
+				+ bprice + ", bdate=" + bdate + "]";
+	}
+
+}
